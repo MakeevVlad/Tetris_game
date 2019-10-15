a2e2f67c0d6256dfb7a534b8f34c6f711ce14c1f
Vladislav <MakeevVlad>
2019-10-15 14:18:53 +0300

    Tennis has been organised. But lots of bugs are still available

--- kernel.py
+++ kernel.py
@@ -1,12 +1,15 @@
 from tkinter import *
-
+import random as rnd
 
 root = Tk()
 root.geometry('800x600')
-field = [800, 600]
-
 canv = Canvas(root, bg = 'white')
 canv.pack(fill=BOTH, expand=1)
+field = [400, 300]
+
+polygon = canv.create_polygon(0, 0, field[0], field[1])
+
+
 
 class Ball:
     def __init__(self, state=[0, 0, 0, 0], color='red', r=10):
@@ -24,24 +27,45 @@ class Ball:
         self.pos[0] + self.r, self.pos[1] + self.r, 
         fill = self.color)
 
-    def move_ball(self):
-        self = hit(self)
+    def move_ball(self, platform):
+        self = hit(self, platform)
         self.pos = pos_step(self.pos, self.vel)
         self.vel = vel_step(self.vel, self.pos)
 
 #changes ball's velocity due to the different collisionss
-def hit(ball):
+def hit(ball, platform):
+    ball = platform_hit(ball, platform)
+    ball = floor_hit(ball)
     ball = wall_hit(ball)
     return ball
 
 #turnes ball's velocity if it hits the wall
 def wall_hit(ball):
-    #right wall
     if ball.pos[0] <= 0 or ball.pos[0] >= field[0]:
         ball.vel[0]*=-1
     if ball.pos[1] <= 0 or ball.pos[1] >= field[1]:
         ball.vel[1]*=-1
     return ball
+
+def floor_hit(ball):
+    if ball.pos[1] >= field[1]:
+        ball.vel = [0, 0]
+        #the_end(ball)
+    return(ball)
+
+def platform_hit(ball, platform):
+    if (ball.pos[1] + ball.r > field[1] - 25 - platform.sizey and
+    ball.pos[1] <= field[1]-25 and
+    ball.pos[0]>=platform.x  and 
+    ball.pos[0]<=platform.x + platform.sizex):
+        ball.vel[1]*=-1
+        ball.pos[1] = field[1] - 25 - platform.sizey - ball.r
+    return(ball)
+
+
+#Draws game over screen       
+def the_end(ball):
+    end = canv.create_rectangle(0, 0, field[0], field[1], fill = 'red')
 
 #changes coordinates of the ball along the timeline
 def pos_step(pos, vel, dt=0.01):
@@ -56,9 +80,10 @@ def vel_step(vel, pos, dt=0.01):
 #================================================
 
 class Platform:
-    def __init__(self, pos=field[0]*0.5, size=50, color='black', ):
+    def __init__(self, pos=field[0]*0.5, sizey=50, sizex = 10, color='black'):
         self.x = pos
-        self.size = size
+        self.sizey = sizey
+        self.sizex = sizex
         self.color = color
         self.draw_platform
         self.obj = canv.create_rectangle(0, 0, 0, 0)
@@ -67,13 +92,13 @@ class Platform:
     def draw_platform(self):
         canv.delete(self.obj)
         self.obj = canv.create_rectangle(
-            self.x, field[1], self.x+self.size, field[1] - self.size/5,
+            self.x, field[1] - 25, self.x + self.sizey, field[1] - 25 - self.sizex,
             fill = self.color)   
     
     def move_platform_right(self):
-        pass
+        self.x = pos_platform_step(self.x, self.sizey)
     def move_platform_left(self):
-        pass
+        self.x = pos_platform_step(self.x, -self.sizey)
     
 
 #=========================
@@ -84,4 +109,7 @@ def platform_movement_buttons(platform):
     command = platform.move_platform_left,  text = '<--')
     move_platform_right_button.place(x = field[0] - 40, y = field[1] - 40)
     move_platform_left_button.place(x = 40, y = field[1] - 40)
-    
+
+def pos_platform_step(pos, vel, dt = 1):
+    pos += vel*dt
+    return pos
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from kernel import *
 
 
 #initializing ball
-ball = Ball([400, 300, 100, 100], 'blue', 10)
+ball = Ball([200, 150, 20, 20], 'blue', 10)
 platform = Platform()
 platform_movement_buttons(platform)
 #Runs game
 def game():
-    ball.move_ball()
+    ball.move_ball(platform)
     ball.draw_ball()
+    platform.draw_platform()
     root.after(1, game)
 
 game()
